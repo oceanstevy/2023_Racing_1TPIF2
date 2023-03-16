33e230a3d28a5e61ea7abca1aeadab7fecef668c
Jonathan20000 <ConJo971@school.lu>
2023-03-16 10:50:14 +0100

    Update functions.js

--- Front-End/Include_Functions/functions.js
+++ Front-End/Include_Functions/functions.js
@@ -1,3 +1,20 @@
 function getRandom(min, max) {
     return Math.random() * (max - min) + min;
 }
+
+function getSessionIdFromCookies() {
+    let name = "PHPSESSID=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let cs = decodedCookie.split(';');
+    for(let i = 0; i <cs.length; i++) {
+
+        while (cs[i].charAt(0) == ' ') {
+            cs[i] = cs[i].substring(1);
+        }
+
+        if (cs[i].indexOf(name) == 0) {
+            return cs[i].substring(name.length, cs[i].length);
+        }
+    }
+    return SessionID;
+}
