beea162b6c8ce10c18c11065d03ad90cbdc97eb0
yanxu003 <120169850+yanxu003@users.noreply.github.com>
2023-03-22 10:57:45 +0100

    loginsite -> backend and other things

--- Back-End/LoginSite.php
+++ Back-End/LoginSite.php
@@ -1,15 +1,4 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
 
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>login</title>
-</head>
-<script src="Include_Functions/jquery.html"></script>
-<body>
 <h1>Login</h1><br>
         <label for='loginName'>Name: </label>
         <input id='loginName' name='loginName' type='text'><br><br>
@@ -43,7 +32,6 @@
 </script>
 <h1>Register</h1><br>
 <?php
-    require_once 'Include_Functions/functions.js';
     require_once '../Back-End/login.php';
     require_once '../Back-End/Register.php';
 ?>
