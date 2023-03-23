10cf08734e1b8d518faaf1c21a17049a7e387922
yanxu003 <120169850+yanxu003@users.noreply.github.com>
2023-03-23 13:00:18 +0100

    Create Ranking.php

---
+++ Back-End/Ranking.php
@@ -0,0 +1,21 @@
+<?php
+require_once "Functions/Credentials.php";
+require_once "Functions/Functions.php";
+
+$connect = db_Connect();
+
+$query = "SELECT * FROM tblScore ORDER BY dtScore";
+
+$result = mysqli_result($query);
+
+$row = mysqli_fetch_assoc($result);
+
+$output = "<table>
+            <tr><th>Rank</th><th>Spieler</th><th>Punkte</th></tr>";
+
+for ($i = 1; $i <= 10; $i++){
+    $output += "<tr><td>{$i}</td><td>{$row['dtName']}</td><td>{$row['dtScore']}</td></tr>";
+}
+
+
+
