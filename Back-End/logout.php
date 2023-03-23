ffc0b326b92ae7051f32a78f69414a6d70a61939
Kreckelneckel <nscheer35@gmail.com>
2023-03-23 10:04:55 +0100

    Finished Logout

--- Back-End/logout.php
+++ Back-End/logout.php
@@ -1,5 +1,18 @@
 <?php
-
+//	@author Scheer Nicolas
+	
+	if ( isset( $_GET[ 'logout' ] ) ) {
+	
+		if ($_GET['logout'] == true) {
+		
+			logout();
+		
+		}
+	
+	}
+	
+//	Function for logging out
+//	@author Scheer Nicolas
 	function logout() {
 		
 		session_destroy();
