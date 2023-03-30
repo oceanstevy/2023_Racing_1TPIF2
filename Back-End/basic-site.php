1e91bbdb85923dcce1ddc6872afa02c6492945aa
Kreckelneckel <nscheer35@gmail.com>
2023-03-30 10:55:37 +0200

    Button Migration
    Migrated Logout, Settings and Playbutton into Basic-site.
    Created Basic-Site.html
    Separated Frontend and Backend of Basic Site

---
+++ Back-End/basic-site.php
@@ -0,0 +1,76 @@
+<?php
+	$_SESSION["user"] = "Johnny";
+	
+	//@author Scheer Nicolas
+	//Logout Button
+	
+	if (isset($_GET['logout'])) {
+		
+		if ($_GET['logout'] == true) {
+			
+			logout();
+			
+		}
+		
+		//Logout END
+		//Settings Button
+	} else if (isset($_GET['settings'])) {
+		
+		if ($_GET["settings"] == true) {
+			
+			settings();
+			
+		}
+		
+		//Settings Button End
+		
+	} else if ($_GET['play']) {
+		
+		if ($_GET['play'] == true) {
+			
+			play();
+			
+		}
+		
+	} else {
+		
+		error();
+		
+	}
+	
+	//	Function for logging out button
+	//	@author Scheer Nicolas
+	function logout()
+	{
+		echo json_encode(["errorCode" => "success!"]);
+		
+		session_destroy();
+		session_unset();
+		
+		
+	}
+	
+	//Function for Settingsbutton
+	//	@author Scheer Nicolas
+	function settings()
+	{
+	
+	
+	}
+	
+	//Function for Playbutton
+	//	@author Scheer Nicolas
+	function play()
+	{
+	
+	
+	}
+	
+	//Function for Errors
+	//	@author Scheer Nicolas
+	function error()
+	{
+		
+		echo json_encode(["errorCode" => "Error!"]);
+		
+	}
