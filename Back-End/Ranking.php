1ebe9fb6a45ec28dbdddfb10d3b5260ba48886ac
yanxu003 <120169850+yanxu003@users.noreply.github.com>
2023-03-30 10:02:10 +0200

    Update Ranking.php

--- Back-End/Ranking.php
+++ Back-End/Ranking.php
@@ -2,19 +2,25 @@
 require_once "Functions/Credentials.php";
 require_once "Functions/Functions.php";
 
-$connect = db_Connect();
+if ($_GET['Action'] = "ranking"){
+    $connect = db_Connect();
 
-$query = "SELECT * FROM tblScore ORDER BY dtScore";
+    $query = "SELECT * FROM tblScore ORDER BY dtScore";
 
-$result = mysqli_result($query);
+    $result = mysqli_result($query);
 
-$row = mysqli_fetch_assoc($result);
+    $row = mysqli_fetch_assoc($result);
 
-$output = "<table>
+    $output = "<table>
             <tr><th>Rank</th><th>Spieler</th><th>Punkte</th></tr>";
 
-for ($i = 1; $i <= 10; $i++){
-    $output += "<tr><td>{$i}</td><td>{$row['dtName']}</td><td>{$row['dtScore']}</td></tr>";
+    for ($i = 1; $i <= 10; $i++){
+        $output += "<tr><td>{$i}</td><td>{$row['dtName']}</td><td>{$row['dtScore']}</td></tr>";
+    }
+
+    $output += "</table>";
+
+    echo json_encode (["errorCode" => "error!"]);
 }
 
 
