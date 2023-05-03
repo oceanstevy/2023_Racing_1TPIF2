cf05d859a71fdb46daddded3acb14f4810631f0f
Jonathan20000 <ConJo971@school.lu>
2023-05-03 20:30:05 +0200

    .

--- Front-End/Include_Functions/functions.js
+++ Front-End/Include_Functions/functions.js
@@ -31,6 +31,23 @@ function getSessionIdFromCookies() {
     return false;
 }
 
+function NewMapModelID(mapid,n) {
+    let numberarr = mapid.toString().split('');
+    let xi = parseInt(numberarr[0] + numberarr[1] + numberarr[2]);
+    let yi = parseInt(numberarr[3] + numberarr[4] + numberarr[5]);
+
+    if (n === "x++"){
+        xi++
+    } else if (n === "y++"){
+        yi++
+    } else if (n === "x--"){
+        xi--
+    } else if (n === "y--"){
+        yi--
+    }
+    return xi +""+ yi
+}
+
 function SetRNColor() {
     $.get("https://www.thecolorapi.com/id?rgb=rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")", function (color) {
         $("th").css("color", color.hex.value);
