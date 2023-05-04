55e5662c7ff30a66fe6d2927e06ceed35cc0a314
Kreckelneckel <nscheer35@gmail.com>
2023-05-04 14:58:09 +0200

    Update admin_profile.php
    Working on Admin Access

--- Back-End/admin_profile.php
+++ Back-End/admin_profile.php
@@ -3,37 +3,38 @@
 	//Author: Nicolas
 	
 	session_start();
+	if (!isset($_SESSION["user"])) {
+	
+	
+	
+	}
 	
 	require_once "Functions/Credentials.php";
 	require_once "Functions/Functions.php";
 	require_once "Query/Querys.php";
 	
 	$dbc = db_Connect();
-	$usernameProfile = $_SESSION['user'];
 //	echo $usernameProfile;
 	$permGroup = 2;
-
+	echo $_SESSION["user"];
 	$checkAdmin = $dbc->prepare(checkIfAdmin());
-	$checkAdmin->bind_param('is',$permGroup, $usernameProfile);
+	$checkAdmin->bind_param('is',$permGroup, $_SESSION["user"]);
 	$checkAdmin->execute();
 
 	$result = $checkAdmin->get_result();
-
-	for($i = 0; $i < mysqli_num_rows ($result); $i++) {
-
-		$row = mysqli_fetch_assoc ($result);
-
-		if ($row["fiPermissionGroup"] === 2) {
+	
+		if (mysqli_num_rows ($result) == 1) {
 		
 		
 		
 		} else {
-		
+			header("Refresh: 1");
 			
-		
+//			header("Location: Github/2023_racing_1TPIF2/Front-End/basic-site.html");
+//			header("Refresh: 1");
 		}
 		
-	}
+	
 	
 ?>
 
